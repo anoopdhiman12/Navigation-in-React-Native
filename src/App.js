96eb7db5b73d0cd99822095729e659bef88a5c87
anoopdhiman12 <projectmanager1221@gmail.com>
2018-01-17 13:26:01 +0530

    installing firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducer'
 
 class App extends Component {
+  componentWillMount(){
+    var config = {
+      apiKey: 'AIzaSyCMnYtqf2F7KQRLoA1TpSvhHbYRmNe6isw',
+      authDomain: 'manager-1a100.firebaseapp.com',
+      databaseURL: 'https://manager-1a100.firebaseio.com',
+      projectId: 'manager-1a100',
+      storageBucket: 'manager-1a100.appspot.com',
+      messagingSenderId: '602280567305'
+    };
+    firebase.initializeApp(config);
+  };
   render() {
     return (
       <Provider store={createStore(reducers)}>
