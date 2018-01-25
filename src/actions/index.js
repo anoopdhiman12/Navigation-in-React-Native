ee5e20c678a5f82dce867ac3875c9e097022e8b9
anoopdhiman12 <anoopdhiman12@gmail.com>
2018-01-25 17:00:53 +0530

    navigation to employeList component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { 
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -44,6 +45,8 @@ export const emailChanged = (text) => {
     dispatch({
       type: LOGIN_USER_SUCCESS,
       payload: user
-    })
+    });
+
+    Actions.main();
   };
 
